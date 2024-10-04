c156a52ce2518e454522346ec87a4682f954592e
Thanapat Thipthiangthae <caveman.pk@gmail.com>
2024-10-04 10:51:27 +0700

    commit
    -

--- pscp/is_prime_LARGER.py
+++ pscp/is_prime_LARGER.py
@@ -1,12 +1,10 @@
 """is_prime_LARGER"""
 def is_prime(n):
     """checking if the number is prime number"""
-    if n == 1: #Even nums won't be prime
+    if n <= 1: #Even nums won't be prime
         return "False"
-    if n == 2:
-        return "True"
-    if not n % 2:
-        return "False"
+    # if not n % 2:
+    #     return "False"
     for divisor in range(3, int(n**0.5)+1, 2):
         if not n % divisor:
             return "False"
